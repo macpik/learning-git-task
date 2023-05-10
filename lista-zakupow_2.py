61245c26c5f0c4fa938bfc802314f2f6c6107416
Maciek Pikula <maciej.pikula@icloud.com>
2023-05-10 14:35:10 +0100

    add loop and count

--- lista-zakupow_2.py
+++ lista-zakupow_2.py
@@ -1,1 +1,5 @@
 shopping_dict = {"piekarnia": ["chleb", "bułki", "pączek"], "warzywniak": ["marchew", "seler", "rukola"]}
+count = 0
+for key,value in shopping_dict.items():
+    if isinstance (value, list) :
+      count += len(value)
